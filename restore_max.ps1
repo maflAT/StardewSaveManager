e9b43b07a69696cd64a106922beacde15f974910
maflAT <13979550+maflAT@users.noreply.github.com>
2022-12-28 20:04:33 +0100

    Created Stardew Save Manager

---
+++ restore_max.ps1
@@ -0,0 +1,4 @@
+py '.\sdsm.py' r -s 'Max'
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
---
+++ save_max.ps1
@@ -0,0 +1,4 @@
+py '.\sdsm.py' s -s 'Max'
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
