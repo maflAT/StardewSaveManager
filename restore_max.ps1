8ad05bfed9896fba5d18b80226ac6b133649f5ef
maflAT <13979550+maflAT@users.noreply.github.com>
2024-05-03 14:58:21 +0200

    removed command line scripts

--- restore_max.ps1
+++
@@ -1,4 +0,0 @@
-py -s '.\sdsm.py' r -s 'Max'
-
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
--- save_max.ps1
+++
@@ -1,4 +0,0 @@
-py -s '.\sdsm.py' s -s 'Max'
-
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
